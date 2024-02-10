21929d1ad14daf0a4333b2c52d2aaf2bed69099a
Victor Santos <santtosvictor42@gmail.com>
2024-02-09 22:01:34 -0300

    chore: added cache

--- src/main/java/taskEasy/demo/TaskEasyApplication.java
+++ src/main/java/taskEasy/demo/TaskEasyApplication.java
@@ -2,8 +2,10 @@ package taskEasy.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TaskEasyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskEasyApplication.class, args);
